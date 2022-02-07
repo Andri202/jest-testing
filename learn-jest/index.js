86f017378fd8867f611cd016dcc42be5eca11ad3
DESKTOP-3C9FMBC\0780 <andrign20@gmail.com>
2022-02-07 09:13:58 +0700

    jest unit test

---
+++ learn-jest/index.js
@@ -0,0 +1,23 @@
+function letterCount(str, char) {
+    let count = 0;
+    for (let letter of str) {
+      if (letter === char) {
+        count++;
+      }
+    }
+    return count;
+}
+
+test("arithmetic", function() {
+    expect(4 + 4).toBeGreaterThan(7);
+    expect(4 + 4).toBeLessThan(9);
+});
+
+test("references", function() {
+    var arr = [1, 2, 3];
+    expect(arr).toEqual([1, 2, 3]);
+    expect(arr).not.toBe([1, 2, 3]); // since === doesn't do deep comparison!
+    expect(arr).toContain(1);
+});
+
+module.exports = letterCount;
